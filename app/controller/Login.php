c7f117de6d5b55216adc7b7146dd6f1c68ff2c5c
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2026-05-07 21:43:05 -0400

    Commit

--- app/controller/Login.php
+++ app/controller/Login.php
@@ -17,4 +17,13 @@ final class Login extends Base
             var_dump($e->getMessage());
         }
     }
+    public function authenticate($request, $response)
+    {
+        try {
+            $data = $request->getParsedBody();
+            var_dump($data);
+        } catch (\Exception $e) {
+            var_dump($e->getMessage());
+        }
+    }
 }
