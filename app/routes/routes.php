e7a105d1233bcf4b99a53d90261e18dce569022c
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2026-05-11 17:22:54 -0400

    Commit

--- app/routes/routes.php
+++ app/routes/routes.php
@@ -6,7 +6,6 @@ $app->get('/', app\controller\Home::class . ':home')->add(app\middleware\Middlew
 $app->get('/home', app\controller\Home::class . ':home')->add(app\middleware\Middleware::web());
 $app->get('/login', app\controller\Login::class . ':login')->add(app\middleware\Middleware::web());
 
-
 $app->group('/cliente', function (Slim\Routing\RouteCollectorProxy $group) {
     $group->get('/lista', app\controller\Customer::class . ':list');
     $group->get('/detalhes/{id}', app\controller\Customer::class . ':details');
